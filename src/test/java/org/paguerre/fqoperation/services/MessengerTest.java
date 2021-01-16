497c950739135b0d400e101569b67fd70fb74106
paguerre3 <camila.milagros.aguerre@gmail.com>
2021-01-16 03:39:00 -0300

    tdd test messages

--- src/test/java/org/paguerre/fqoperation/services/MessengerTest.java
+++ src/test/java/org/paguerre/fqoperation/services/MessengerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.paguerre.fqoperation.models.RebelSatellite;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -19,5 +20,37 @@ public class MessengerTest {
 				() -> messenger.getMessage((String[][]) null),
 				"Expected getMessage() to throw IllegalArgumentException -> 'All messagers are null or empty', but it didn't");
 		assertEquals("All messagers are null or empty.", thrown.getMessage());
+	}
+
+	@Test
+	public void getMessageFromFQOperation() {
+		RebelSatellite rb1 = new RebelSatellite("kenobi", null, 0.0, new String[] { "este", "", "", "mensaje", "" });
+		RebelSatellite rb2 = new RebelSatellite("skywalker", null, 0.0, new String[] { "", "es", "", "", "secreto" });
+		RebelSatellite rb3 = new RebelSatellite("sato", null, 0.0, new String[] { "este", "", "un", "", "" });
+		String message = messenger.getMessage(rb1.getMessageFromSource(), rb2.getMessageFromSource(),
+				rb3.getMessageFromSource());
+		assertEquals("este es un mensaje secreto", message);
+	}
+
+	@Test
+	public void getMessageWithErroneousForms() {
+		RebelSatellite rb1 = new RebelSatellite("kenobi", null, 0.0, new String[] { "este", "", "", "mensaje", "" });
+		RebelSatellite rb2 = new RebelSatellite("skywalker", null, 0.0, new String[] { "", "es", "", "", "secreto" });
+		RebelSatellite rb3 = new RebelSatellite("sato", null, 0.0, new String[] { "este", "", "un", "", "", "" });
+		String message = messenger.getMessage(rb1.getMessageFromSource(), rb2.getMessageFromSource(),
+				rb3.getMessageFromSource());
+		assertEquals("este es un mensaje secreto", message);
+	}
+
+	@Test
+	public void getMessageWithMoreErroneousForms() {
+		RebelSatellite rb1 = new RebelSatellite("kenobi", null, 0.0,
+				new String[] { "", "este", "es", "un", "mensaje" });
+		RebelSatellite rb2 = new RebelSatellite("skywalker", null, 0.0,
+				new String[] { "", "este", "", "un", "mensaje" });
+		RebelSatellite rb3 = new RebelSatellite("sato", null, 0.0, new String[] { "", "", "es", "", "mensaje" });
+		String message = messenger.getMessage(rb1.getMessageFromSource(), rb2.getMessageFromSource(),
+				rb3.getMessageFromSource());
+		assertEquals("este es un mensaje", message);
 	}
 }
