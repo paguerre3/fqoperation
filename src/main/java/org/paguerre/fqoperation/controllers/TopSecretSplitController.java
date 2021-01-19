1fc651dffd77381b9adef09ea5d21199994b1256
paguerre3 <camila.milagros.aguerre@gmail.com>
2021-01-19 02:48:15 -0300

    update v2 endpoints

--- src/main/java/org/paguerre/fqoperation/controllers/TopSecretSplitController.java
+++ src/main/java/org/paguerre/fqoperation/controllers/TopSecretSplitController.java
@@ -45,7 +45,7 @@ public class TopSecretSplitController {
 	 * 
 	 * @return ResponseEntity<?> including transporter information
 	 */
-	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE }, value = "/{name}")
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<?> processTopSecretSplit() {
 		ResponseEntity<?> retVal = ResponseEntity.notFound().build();
 		Transporter transporter = topSecretSplitSvc.find();
